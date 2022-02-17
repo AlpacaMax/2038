437831d298da805f405d1dd2cb648673cfcd8816
Maxwell Yang <gabrielyang233@outlook.com>
2022-02-16 23:06:37 -0500

    ADD Core Functionalities and Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.min.css';
+import React, { useEffect, useState } from 'react';
+
+function to32bit(val) {
+  return val & (2**31 - 1);
+}
+
+function getCurrTimestamps() {
+  const ts = parseInt(Date.now() / 1000);
+  return [ts, to32bit(ts)];
+}
 
 function App() {
+  const [timestamps, setTimestamps] = useState(getCurrTimestamps());
+  const date64 = new Date(timestamps[0] * 1000);
+  const date32 = new Date(timestamps[1] * 1000);
+  const dateFormater = new Intl.DateTimeFormat(
+    'en-GB',
+    {
+      dateStyle: 'full',
+      timeStyle: 'long',
+    }
+  )
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimestamps(getCurrTimestamps());
+    }, 1000);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App columns">
+      <div className="column">
+        <div className="is-size-4">
+          64-bit Version
+        </div>
+        <div className="is-size-5">
+          { timestamps[0] }
+        </div>
+        <div className="is-size-3">
+          { dateFormater.format(date64) }
+        </div>
+         
+      </div>
+      <div className="column">
+        <div className="is-size-4">
+          32-bit Version
+        </div>
+        <div className="is-size-5">
+          { timestamps[1] }
+        </div>
+        <div className="is-size-3">
+          { dateFormater.format(date32) }
+        </div>
+      </div>
     </div>
   );
 }
