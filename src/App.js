987dd155b79c35ee9fbd9935fa73641b6610ed0c
Maxwell Yang <gabrielyang233@outlook.com>
2022-02-19 00:21:27 -0500

    ADD More Styling and description of the bug

--- src/App.js
+++ src/App.js
@@ -28,31 +28,44 @@ function App() {
   });
 
   return (
-    <div className="App container is-fluid">
-      <div className="columns">
+    <div className="App container is-fluid has-background-light">
+      <div className="columns m-0">
         <div className="column">
-          <div className="is-size-4">
+          <div className="is-size-4 has-text-dark">
             64-bit Version
           </div>
-          <div className="is-size-5">
+          <div className="is-size-5 has-text-primary">
             { timestamps[0] }
           </div>
-          <div className="is-size-3 has-text-weight-bold">
+          <div className="is-size-3 has-text-weight-bold has-text-dark">
             { dateFormater.format(date64) }
           </div>
           
         </div>
         <div className="column">
-          <div className="is-size-4">
+          <div className="is-size-4 has-text-dark">
             32-bit Version
           </div>
-          <div className="is-size-5">
+          <div className="is-size-5 has-text-primary">
             { timestamps[1] }
           </div>
-          <div className="is-size-3 has-text-weight-bold">
+          <div className="is-size-3 has-text-weight-bold has-text-dark">
             { dateFormater.format(date32) }
           </div>
         </div>
+      </div>
+
+      <div class="content has-text-dark m-3">
+        <p>
+          On 03:14:08 UTC 19 January 2038, timestamps that are stored in signed 32-bit integers
+          will overflow, causing it to represent 20:45:52 UTC on 13 December 1901. At that point 
+          basically every device should be using signed 64-bit integers
+          to represent time. The only concerns are some legacy systems, especially embedded systems,
+          will still be using signed 32-bit integers, causing potentially fatal errors just like
+          the ones in year 2000. If you think the system you are 
+          maintaining could have this problem, please fix them as soon as possible before it
+          causes any damage.
+        </p>
       </div>
     </div>
   );
